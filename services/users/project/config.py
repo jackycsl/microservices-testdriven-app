9c3f94ede72fd24f4a90636ed15aa9ab2b3920e2
zuiryuu <jcslmu@gmail.com>
2018-12-10 18:13:04 +0800

    set up base flask architecture and define first service

---
+++ services/users/project/config.py
@@ -0,0 +1,18 @@
+class BaseConfig:
+    """Base Configuration"""
+    TESTING = False
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development Configuration"""
+    pass
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    pass
