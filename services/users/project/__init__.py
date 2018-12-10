44f1e334bb7d080f9cb1215200bc4c5d247ba8ff
zuiryuu <jcslmu@gmail.com>
2018-12-10 18:48:46 +0800

    containarize flask app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,8 +1,13 @@
+import os
 from flask import Flask, jsonify
 
 # instantiate the app
 app = Flask(__name__)
-app.config.from_object('project.config.DevelopmentConfig')
+
+# set config
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
 
 @app.route('/users/ping', methods=['GET'])
 def pingpong():
