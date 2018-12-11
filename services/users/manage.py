47942724807208442ec6c8b2066ac5c9af86ce2f
zuiryuu <jcslmu@gmail.com>
2018-12-11 21:47:44 +0800

    refactor app with Blueprints

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,10 +1,12 @@
-from flask.cli import FlaskGroup
 import unittest
 
-from project import app
+from flask.cli import FlaskGroup
 
+from project import create_app, db
+from project.api.models import User
 
-cli = FlaskGroup(app)
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
 
 @cli.command()
 def recreate_db():
