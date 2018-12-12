adc13800b4025a119db19db8e88259b29a43de21
zuiryuu <jcslmu@gmail.com>
2018-12-12 17:54:35 +0800

    add users server side template

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -137,6 +137,40 @@ class TestUserService(BaseTestCase):
             self.assertIn('wicky@demo.com', data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
+    def test_main_no_user(self):
+        """Ensure main route behaves correctly when no users have been
+        added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'All Users', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been
+        added to the database."""
+        add_user('jacky', 'jacky@demo.com')
+        add_user('wicky', 'wicky@demo.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'jacky', response.data)
+            self.assertIn(b'wicky', response.data)
+
+    def test_main_add_user(self):
+        """Ensure a new user can be added to the database"""
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='jacky', email='jacky@demo.com'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'jacky', response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
