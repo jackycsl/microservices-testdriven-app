71ae742c73bd0defa0a462f5852a60658bd7a695
zuiryuu <jcslmu@gmail.com>
2018-12-20 17:12:48 +0800

    Add authentication & user status

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import About from './components/About';
 import NavBar from './components/NavBar';
 import Form from './components/Form';
 import Logout from './components/Logout';
+import UserStatus from './components/UserStatus';
 
 
 class App extends Component {
@@ -108,7 +109,10 @@ class App extends Component {
   render() {
     return(
       <div>
-        <NavBar title={this.state.title} />
+        <NavBar
+          title={this.state.title}
+          isAuthenticated={this.state.isAuthenticated}
+        />
         <section className="section">
           <div className="container">
             <div className="columns">
@@ -155,6 +159,11 @@ class App extends Component {
                       isAuthenticated={this.state.isAuthenticated}
                     />
                   )} />
+                  <Route exact path='/status' render={() => (
+                    <UserStatus
+                      isAuthenticated={this.state.isAuthenticated}
+                    />
+                  )}/>
                 </Switch>
               </div>
             </div>
