746ed1be183fce3db5bb28d415cbc0aad00aa7bb
zuiryuu <jcslmu@gmail.com>
2018-12-24 12:38:23 +0800

    add flash messaging

--- cypress/integration/register.test.js
+++ cypress/integration/register.test.js
@@ -39,7 +39,7 @@ describe('Register', () => {
     cy.get('.validation-list > .error').contains(
       'Password must be greater than 10 characters.');
 
-  })
+  });
 
   it('should allow a user to register', () => {
 
@@ -64,6 +64,60 @@ describe('Register', () => {
         .get('.navbar-item').contains('Register').should('not.be.visible');
     })
 
-  })
+  });
+
+  it('should throw an error if the username is taken', () => {
+
+    // register user with duplicate user name
+    cy
+      .visit('/register')
+      .get('input[name="username"]').type(username)
+      .get('input[name="email"]').type(`${email}unique`)
+      .get('input[name="password"]').type(password)
+      .get('input[type="submit"]').click();
+
+    // assert user registration failed
+    cy.contains('All Users').should('not.be.visible');
+    cy.contains('Register');
+    cy.get('.navbar-burger').click();
+    cy.get('.navbar-menu').within(() => {
+      cy
+        .get('.navbar-item').contains('User Status').should('not.be.visible')
+        .get('.navbar-item').contains('Log Out').should('not.be.visible')
+        .get('.navbar-item').contains('Log In')
+        .get('.navbar-item').contains('Register');
+    });
+    cy
+      .get('.notification.is-success').should('not.be.visible')
+      .get('.notification.is-danger').contains('Login failed.');
+
+  });
+
+  it('should throw an error if the email is taken', () => {
+
+    // register user with duplicate email
+    cy
+      .visit('/register')
+      .get('input[name="username"]').type(`${username}unique`)
+      .get('input[name="email"]').type(email)
+      .get('input[name="password"]').type(password)
+      .get('input[type="submit"]').click();
+
+    // assert user registration failed
+    cy.contains('All Users').should('not.be.visible');
+    cy.contains('Register');
+    cy.get('.navbar-burger').click();
+    cy.get('.navbar-menu').within(() => {
+      cy
+        .get('.navbar-item').contains('User Status').should('not.be.visible')
+        .get('.navbar-item').contains('Log Out').should('not.be.visible')
+        .get('.navbar-item').contains('Log In')
+        .get('.navbar-item').contains('Register');
+    });
+    cy
+      .get('.notification.is-success').should('not.be.visible')
+      .get('.notification.is-danger').contains('Login failed.');
+
+  });
 
 });
--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -8,6 +8,7 @@ import NavBar from './components/NavBar';
 import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
+import Message from './components/Message';
 
 
 class App extends Component {
@@ -17,9 +18,13 @@ class App extends Component {
       users: [],
       title: 'Virtuosity',
       isAuthenticated: false,
+      messageName: null,
+      messageType: null,
     };
     this.logoutUser = this.logoutUser.bind(this);
     this.loginUser = this.loginUser.bind(this);
+    this.createMessage = this.createMessage.bind(this);
+    this.removeMessage = this.removeMessage.bind(this);
   };
 
   componentWillMount() {
@@ -47,7 +52,25 @@ class App extends Component {
     window.localStorage.setItem('authToken', token);
     this.setState({ isAuthenticated: true});
     this.getUsers();
+    this.createMessage('Welcome!', 'success');
   }
+
+  createMessage(name='Sanity Sheck', type='success'){
+    this.setState({
+      messageName: name,
+      messageType: type
+    });
+    setTimeout(() => {
+      this.removeMessage();
+    }, 3000)
+  };
+
+  removeMessage() {
+    this.setState({
+      messageName: null,
+      messageType: null
+    });
+  };
 
   render() {
     return(
@@ -58,6 +81,13 @@ class App extends Component {
         />
         <section className="section">
           <div className="container">
+            { this.state.messageName && this.state.messageType &&
+              <Message
+                messageName={this.state.messageName}
+                messageType={this.state.messageType}
+                removeMessage={this.removeMessage}
+              />
+            }
             <div className="columns">
               <div className="column is-half">
                 <br/>
@@ -73,6 +103,7 @@ class App extends Component {
                       formType={'Register'}
                       isAuthenticated={this.state.isAuthenticated}
                       loginUser={this.loginUser}
+                      createMessage={this.createMessage}
                     />
                   )} />
                   <Route exact path='/login' render={() => (
@@ -80,6 +111,7 @@ class App extends Component {
                       formType={'Login'}
                       isAuthenticated={this.state.isAuthenticated}
                       loginUser={this.loginUser}
+                      createMessage={this.createMessage}
                     />
                   )} />
                   <Route exact path='/logout' render={() => (
